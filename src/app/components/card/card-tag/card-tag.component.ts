9df1b57e6d2242a7fb7d8b2bffc7924984108420
Rick <rickbatera2012@gmail.com>
2023-02-06 11:40:24 -0300

    create card-tag

---
+++ src/app/components/card/card-tag/card-tag.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-tag',
+  templateUrl: './card-tag.component.html',
+  styleUrls: ['./card-tag.component.css']
+})
+export class CardTagComponent {
+
+}
