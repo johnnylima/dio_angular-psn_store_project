80d18ecb50b4017b2ea4b217b108cb1280917224
Rick <rickbatera2012@gmail.com>
2023-02-06 11:40:40 -0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CardComponent } from './components/card/card.component';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { CardTagComponent } from './components/card/card-tag/card-tag.component';
+import { CardDetailComponent } from './components/card/card-detail/card-detail.component';
+import { CardPriceComponent } from './components/card/card-price/card-price.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     CardComponent,
-    MenuBarComponent
+    MenuBarComponent,
+    CardTagComponent,
+    CardDetailComponent,
+    CardPriceComponent
   ],
   imports: [
     BrowserModule,
