133f48090b07cb6fdae431992688dc74b2f76bc0
Rick <rickbatera2012@gmail.com>
2023-02-06 15:28:48 -0300

    update card-price component

--- src/app/components/card/card-price/card-price.component.ts
+++ src/app/components/card/card-price/card-price.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-price',
   templateUrl: './card-price.component.html',
   styleUrls: ['./card-price.component.css']
 })
-export class CardPriceComponent {
+export class CardPriceComponent implements OnInit{
+  price:string="R$ 15,99"
+  ngOnInit(): void {
+  }
+
 
 }
