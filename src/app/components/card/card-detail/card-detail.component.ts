b0c38f174ee4ebc15b99687bb31d389da9fdac51
Rick <rickbatera2012@gmail.com>
2023-02-06 15:14:25 -0300

    update card-detail component

--- src/app/components/card/card-detail/card-detail.component.ts
+++ src/app/components/card/card-detail/card-detail.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-detail',
   templateUrl: './card-detail.component.html',
   styleUrls: ['./card-detail.component.css']
 })
-export class CardDetailComponent {
+export class CardDetailComponent implements OnInit{
+
+title:string="Play It Now!"
+type:string="Full Game"
+classifications:string="PS4"
+
+  ngOnInit(): void {
+  }
 
 }
