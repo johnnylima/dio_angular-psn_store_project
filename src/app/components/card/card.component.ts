701d0395a0deaf264220c6ecdae7dd3482128725
Rick <rickbatera2012@gmail.com>
2023-02-06 14:37:55 -0300

    update card component

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent {
+export class CardComponent implements OnInit{
+  imgUrl = "../../../assets/bt-1.jpg";
+  ngOnInit(): void {
+  }
 
 }
